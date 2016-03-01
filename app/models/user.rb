8f1798ebd725a335afd3e446603008d820f0b522
imoken <imototakeshi@gmail.com>
2016-03-01 12:12:44 +0900

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   has_many :blogs, :dependent => :delete_all
+  has_many :comments
   
   mount_uploader :avatar, AvatarUploader
 
