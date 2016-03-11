140c43eea9115f74f9d6d50d73c4e72deba63e0e
imoken <imoto@niocle.com>
2016-03-11 14:27:55 +0900

    def following def followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,17 @@ class UsersController < ApplicationController
     @users = User.find(params[:id])
   end
 
+  def following
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+   def followers
+     @user = User.find(:params[:id])
+     @users = @user.followers.paginate(page: params[:page])
+     render 'show_follow'
+   end
+
 end
 
