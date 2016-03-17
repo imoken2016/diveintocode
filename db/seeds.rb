defbea0f25570316f4176cdb63f0085382bd7a1a
imoken <imoto@niocle.com>
2016-03-17 14:53:29 +0900

    add pagenation

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
 #Blog.create([{ title: 'できた！', content: '本文を入れるよ',user_id: user.id }])
 #Title.create(:name => '観察日記', :sales_date => '2011-11-14', :price => 1000)
 
-30.times do |no|
+100.times do |no|
   user = User.create(name: "名無し太朗#{no}号", email: "hogehoge#{no}@example.com", password: '12345678', password_confirmation: '12345678', confirmed_at: Time.now,uid: no, provider: no)
   Blog.create(title: "できたよ！#{no}", content: "本文を入れるよ", user_id: user.id )
 end
